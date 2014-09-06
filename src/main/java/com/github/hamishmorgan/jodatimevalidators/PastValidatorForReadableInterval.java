b9fa7b2d5ca69daf96824b7500ad0dae9706a909
Hamish Morgan <hamish.morgan@gmail.com>
2014-09-06 14:26:44 +0100

    Added past validator for intervals

---
+++ src/main/java/com/github/hamishmorgan/jodatimevalidators/PastValidatorForReadableInterval.java
@@ -0,0 +1,26 @@
+package com.github.hamishmorgan.jodatimevalidators;
+
+import org.joda.time.ReadableInterval;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Past;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Validator to constrain {@link org.joda.time.ReadableInterval} in the past.
+ * <p/>
+ * Both the start and end instants must be in the past. Verified to work with all known concrete implements:
+ * {@link org.joda.time.Interval}, and {@link org.joda.time.MutableInterval}. Null instants are considered valid.
+ */
+@SuppressWarnings("deprecation")
+public class PastValidatorForReadableInterval extends AbstractCurrentTimeRelativeValidator<Past, ReadableInterval> {
+
+    @Override
+    public boolean isValid(@Nullable ReadableInterval value, @Nonnull ConstraintValidatorContext context) {
+        checkNotNull(context, "context");
+        return value == null || value.isBefore(getCurrentDateTime());
+    }
+}
---
+++ src/test/java/com/github/hamishmorgan/jodatimevalidators/PastValidatorForReadableIntervalTest.java
@@ -0,0 +1,190 @@
+package com.github.hamishmorgan.jodatimevalidators;
+
+import com.google.common.base.Supplier;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.MutableInterval;
+import org.joda.time.ReadableInterval;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Past;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PastValidatorForReadableIntervalTest {
+
+    @Mock
+    private Supplier<DateTime> currentDateTimeSupplier;
+
+    @InjectMocks
+    private PastValidatorForReadableInterval validator;
+
+    @Mock
+    private Past constraintAnnotation;
+
+    @Mock
+    private ConstraintValidatorContext constraintValidatorContext;
+
+    @Before
+    public void initializeMocks() {
+        DateTime fixedDateTime = new DateTime(2014, 9, 6, 10, 17, 1, 0);
+        when(currentDateTimeSupplier.get()).thenReturn(fixedDateTime);
+    }
+
+    @Test
+    public void whenDefaultConstructorCalled_thenNoExceptionThrown() {
+        new PastValidatorForReadableInstant();
+    }
+
+    @Test
+    public void givenValidConstraintAnnotation_whenInitialize_thenZeroInteractions() {
+        validator.initialize(constraintAnnotation);
+        verifyZeroInteractions(constraintAnnotation);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void givenNullConstraintAnnotation_whenInitialize_thenThrowsNPE() {
+        validator.initialize(null);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void givenNullConstraintValidatorContext_whenIsValid_thenNPE() {
+        validator.isValid(null, null);
+    }
+
+    @Test
+    public void givenValidConstraintValidatorContext_whenIsValid_thenZeroInteractions() {
+        validator.isValid(null, constraintValidatorContext);
+        verifyZeroInteractions(constraintAnnotation);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void givenNullReadableInstant_whenIsValid_thenReturnTrue() {
+        ReadableInterval instant = null;
+        boolean valid = validator.isValid(instant, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenStartAndEndAreInThePast_whenIsValid_thenReturnTrue() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get().minusSeconds(2),
+                currentDateTimeSupplier.get().minusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenStartIsInThePastAndEndIsNow_whenIsValid_thenReturnTrue() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get().minusSeconds(1),
+                currentDateTimeSupplier.get());
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenStartIsNowAndEndIsInTheFuture_whenIsValid_thenReturnFalse() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get(),
+                currentDateTimeSupplier.get().plusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    public void givenStartAndEndIsNow_whenIsValid_thenReturnTrue() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get(),
+                currentDateTimeSupplier.get());
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenStartInThePatAndEndIsInTheFuture_whenIsValid_thenReturnFalse() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get().minusSeconds(1),
+                currentDateTimeSupplier.get().plusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    public void givenStartAndEndInTheFuture_whenIsValid_thenReturnFalse() {
+        Interval interval = new Interval(
+                currentDateTimeSupplier.get().plusSeconds(1),
+                currentDateTimeSupplier.get().plusSeconds(2));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+
+    @Test
+    public void givenMutableIntervalStartAndEndAreInThePast_whenIsValid_thenReturnTrue() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get().minusSeconds(2),
+                currentDateTimeSupplier.get().minusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenMutableIntervalStartIsInThePastAndEndIsNow_whenIsValid_thenReturnTrue() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get().minusSeconds(1),
+                currentDateTimeSupplier.get());
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenMutableIntervalStartIsNowAndEndIsInTheFuture_whenIsValid_thenReturnFalse() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get(),
+                currentDateTimeSupplier.get().plusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    public void givenMutableIntervalStartAndEndIsNow_whenIsValid_thenReturnTrue() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get(),
+                currentDateTimeSupplier.get());
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    public void givenMutableIntervalStartInThePatAndEndIsInTheFuture_whenIsValid_thenReturnFalse() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get().minusSeconds(1),
+                currentDateTimeSupplier.get().plusSeconds(1));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    public void givenMutableIntervalStartAndEndInTheFuture_whenIsValid_thenReturnFalse() {
+        MutableInterval interval = new MutableInterval(
+                currentDateTimeSupplier.get().plusSeconds(1),
+                currentDateTimeSupplier.get().plusSeconds(2));
+        boolean valid = validator.isValid(interval, constraintValidatorContext);
+        assertThat(valid).isFalse();
+    }
+
+
+}
